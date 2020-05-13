dc32934b617ee250cefd199e05581f21d51a6c07
Anna Savina <sav.ahha321@gmail.com>
2020-05-13 14:35:20 +0300

    карта

--- TransLionApp/TransLionApp/TransLionApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ TransLionApp/TransLionApp/TransLionApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"4db7f727-98fd-475d-ab21-8879cf00d1c8"
+	.asciz	"a5856911-1200-46bf-ba1d-b869a3f199e5"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
