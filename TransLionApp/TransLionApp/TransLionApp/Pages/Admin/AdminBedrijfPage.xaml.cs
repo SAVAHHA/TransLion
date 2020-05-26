61c6143efbb4400c7c1f00624314ab3a69730d87
lenavolkovs <58178482+lenavolkovs@users.noreply.github.com>
2020-05-27 03:02:46 +0400

    нс

---
+++ TransLionApp/TransLionApp/TransLionApp/Pages/Admin/AdminBedrijfPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TransLionApp.Pages.Admin
+{
+    public partial class AdminBedrijfPage : ContentPage
+    {
+        public AdminBedrijfPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- TransLionApp/TransLionApp/TransLionApp/Pages/Admin/BedrijvenPage.xaml.cs
+++ TransLionApp/TransLionApp/TransLionApp/Pages/Admin/BedrijvenPage.xaml.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransLionApp.Controls;
+using TransLionApp.Data;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TransLionApp.Views;
+
 
 namespace TransLionApp.Pages.Admin
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BedrijvenPage : ContentPage
     {
         public BedrijvenPage()
         {
             InitializeComponent();
         }
+
     }
 }
