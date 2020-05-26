d80c455c5634bc39aa8ab4f127b949ef20de0f29
lenavolkovs <58178482+lenavolkovs@users.noreply.github.com>
2020-05-27 00:40:16 +0400

    ане

---
+++ TransLionApp/TransLionApp/TransLionApp/Pages/User/Mijngegevens.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TransLionApp.Pages.User
+{
+    public partial class Mijngegevens : ContentPage
+    {
+        public Mijngegevens()
+        {
+            InitializeComponent();
+        }
+    }
+}
