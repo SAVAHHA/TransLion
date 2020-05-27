df98cf853c3ec941178a9a20eed58bf93f80ca76
Anna Savina <sav.ahha321@gmail.com>
2020-05-27 10:31:10 +0300

    ооо

--- TransLionApp/TransLionApp/TransLionApp/obj/Debug/netstandard2.0/Pages/Admin/BedrijvenPage.xaml.g.cs
+++ TransLionApp/TransLionApp/TransLionApp/obj/Debug/netstandard2.0/Pages/Admin/BedrijvenPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace TransLionApp.Pages.Admin {
     public partial class BedrijvenPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button plusButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.CollectionView searchResults;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(BedrijvenPage));
+            plusButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "plusButton");
+            searchResults = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "searchResults");
         }
     }
 }
